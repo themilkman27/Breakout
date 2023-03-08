2cc627fc889ab254e02ca363e4c6cab85be1c189
Owen Johnson <owenbjohnson10@gmail.com>
2023-03-08 10:04:58 -0600

    Created Ball Object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,8 +9,53 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var ball = SKShapeNode()
     override func didMove(to view: SKView) {
+        createBackground()
+        resetGame()
         //this stuff happens once when the app opens
+    
+    }
+    
+    func resetGame() {
+        makeBall()
+    }
+    
+    func createBackground() {
+        let stars = SKTexture(imageNamed: "Stars")
+        for i in 0...1 {
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            addChild(starsBackground)
+            let moveDown = SKAction.moveBy(x: 0,y: -starsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0,y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackground.run(moveForever)
+
+        }
+    }
+    func makeBall() {
+        ball.removeFromParent()
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        ball.physicsBody?.isDynamic = false
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.contactTestBitMask)!
+        
+        addChild(ball)
     }
 }
+
+
 
